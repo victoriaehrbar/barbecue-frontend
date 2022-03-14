e935d2d5e5385a1e5243c4eea1e6df93e6d899f5
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-14 17:56:12 -0500

    index

---
+++ src/components/BarbecueIndex.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getBarbecues } from "../redux/actions/barbecueActions";
+import BarbecueListItem from "./BarbecueListItem";
+
+class BarbecueIndex extends React.Component {
+  componentDidMount() {
+    this.props.getBarbecues();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Barbecue Restaurants</h1>
+        {this.props.barbecues.map((barbecue) => (
+          <BarbecueListItem barbecue={barbecue} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ barbecues }) => {
+  return {
+    barbecues: barbecues.all,
+  };
+};
+
+export default connect(mapStateToProps, { getBarbecues })(BarbecueIndex);

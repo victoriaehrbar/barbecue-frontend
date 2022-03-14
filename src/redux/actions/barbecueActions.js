55796072f7bbd27a6c0bad2a820b66fd786a157e
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-14 18:08:37 -0500

    actions

---
+++ src/redux/actions/barbecueActions.js
@@ -0,0 +1,26 @@
+
+export const getBarbecues = () => {
+    return (dispatch) => {
+      fetch(`http://localhost:3001/barbecues`)
+        .then((res) => res.json())
+        .then((bbqs) =>
+          dispatch({ type: "FETCH_BARBECUES_SUCCESS", payload: barbecues })
+        );
+    };
+  };
+  
+  export const createBarbecue = (data) => {
+    return (dispatch) => {
+      fetch(`http://localhost:3001/barbecues`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ barbecue: data }),
+      })
+        .then((res) => res.json())
+        .then((barbecue) =>
+          dispatch({ type: "CREATE_BARBECUE_SUCCESS", payload: barbecue })
+        );
+    };
+  };
